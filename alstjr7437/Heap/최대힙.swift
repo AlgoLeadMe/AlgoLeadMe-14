5ace76b0764dc78dd04537c55fbffc28425241ca
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-08 19:48:59 +0900

    2차시 문제 해결

---
+++ alstjr7437/Heap/최대힙.swift
@@ -0,0 +1,65 @@
+struct Heap {
+    var elements: [Int] = []
+    
+    mutating func insert(node: Int) {
+        self.elements.append(node)
+        
+        var index = elements.count - 1
+        while index > 0 {
+            let parent = (index - 1) / 2
+            
+            if elements[parent] < elements[index] {
+                let temp = elements[parent]
+                elements[parent] = elements[index]
+                elements[index] = temp
+                index = parent
+            } else {
+                break
+            }
+        }
+    }
+    
+    mutating func remove() -> Int? {
+        guard !elements.isEmpty else { return nil }
+        if elements.count == 1 {
+            return elements.removeFirst()
+        }
+
+        let removeValue = elements[0]
+        elements[0] = elements.removeLast()
+        
+        var index = 0
+        while index < elements.count {
+            let left = index * 2 + 1
+            let right = index * 2 + 2
+            var current = index
+            
+            if left < elements.count && elements[left] > elements[current] {
+                current = left
+            }
+            if right < elements.count && elements[right] > elements[current] {
+                current = right
+            }
+            if current == index { break }
+            
+            elements.swapAt(index, current)
+            index = current
+        }
+        
+        return removeValue
+    }
+    
+}
+
+let N = Int(readLine()!)!
+var heap = Heap()
+
+for _ in 0..<N {
+    let input = Int(readLine()!)!
+    if input == 0 {
+        print(heap.remove() ?? 0)
+    } else {
+        heap.insert(node: input)
+    }
+}
+
