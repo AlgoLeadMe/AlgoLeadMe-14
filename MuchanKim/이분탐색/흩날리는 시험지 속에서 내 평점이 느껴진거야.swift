4bd77bbc808c0d92c714ef2483f862986e084d89
Moo <mutopia82@gmail.com>
2025-05-24 13:56:17 +0900

    코드 수정

--- MuchanKim/이분탐색/흩날리는 시험지 속에서 내 평점이 느껴진거야.swift
+++ MuchanKim/이분탐색/흩날리는 시험지 속에서 내 평점이 느껴진거야.swift
@@ -4,32 +4,40 @@ import Foundation
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let group = input[1]
 
-// 문제 최대 개수 20
 let scores = readLine()!.split(separator: " ").map { Int($0)! }
 
-var start = 0
-var end = 20 * 100000
-var answer = -1
-
-while start <= end {
-    let mid = (start + end) / 2
+func canMakeGroups(_ scores: [Int], _ target: Int, _ group: Int) -> Bool {
     var sum = 0
     var count = 0
     
     for score in scores {
         sum += score
-        if sum >= mid {
+        if sum >= target {
             count += 1
             sum = 0
         }
     }
     
-    if count >= group {
-        answer = mid
-        start = mid + 1
-    } else {
-        end = mid - 1
-    }
+    return count >= group
 }
 
-print(answer) 
+func solution(_ scores: [Int], _ group: Int) -> Int {
+    var start = 0
+    var end = scores.reduce(0, +)
+    var res = 0
+    
+    while start <= end {
+        let mid = (start + end) / 2
+        
+        if canMakeGroups(scores, mid, group) {
+            res = mid
+            start = mid + 1
+        } else {
+            end = mid - 1
+        }
+    }
+    
+    return res
+}
+
+print(solution(scores, group)) 
